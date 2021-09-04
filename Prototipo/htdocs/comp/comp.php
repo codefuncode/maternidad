61237d75fbe6b533d04ec1c2373751eb950a60b3
alemandiazcarlos <caleman9791>
2021-09-04 01:27:40 -0400

    Recuperación del fichero formulario. Todo trabajando correctamente, falta trabajar la omisión de la opción lactancia.

--- Prototipo/htdocs/comp/comp.php
+++ Prototipo/htdocs/comp/comp.php
@@ -6,7 +6,7 @@ include_once 'error_server.php';
 include_once 'existe_usuario.php';
 include_once 'bienvenida_inicio.php';
 include_once 'inicia_sesion.php';
-include_once 'formulario_bebe.php';
+include_once 'form/formulario_bebe.php';
 include_once 'perfil_usuario.php';
 include_once 'mensaje_bebe_registro.php';
 // include_once 'inicio.php';
--- Prototipo/htdocs/conn/conn.php
+++ Prototipo/htdocs/conn/conn.php
@@ -1,11 +1,11 @@
 <?php
 
-// $servername = "localhost";
-// $username   = "root";
-// $password   = "";
-// $dbname     = "maternidad";
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "maternidad";
 
-$servername = "sql201.epizy.com";
-$username   = "epiz_27965998";
-$password   = "gTrvbP9EEXxrKyk";
-$dbname     = "epiz_27965998_maternidad";
+// $servername = "sql201.epizy.com";
+// $username   = "epiz_27965998";
+// $password   = "gTrvbP9EEXxrKyk";
+// $dbname     = "epiz_27965998_maternidad";
