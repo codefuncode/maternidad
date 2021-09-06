9d9c961b4670f0f1fd5b7ddbcad3ef4f74421e07
alemandiazcarlos <caleman9791>
2021-09-05 23:06:16 -0400

    Componte para la navegación de la parte  superior de la página fue trasladado a la zona de componentes.

--- Prototipo/htdocs/conn/conn.php
+++ Prototipo/htdocs/conn/conn.php
@@ -5,13 +5,7 @@ $username   = "root";
 $password   = "";
 $dbname     = "maternidad";
 
-$servername = "sql201.epizy.com";
-$username   = "epiz_27965998";
-$password   = "gTrvbP9EEXxrKyk";
-$dbname     = "epiz_27965998_maternidad";
-
-// $servername = "sql451.main-hosting.eu";
-// $username   = "u317430902_pruebas";
-// $password   = "Caleman9791";
-// $dbname     = "u317430902_pruebas";
-// // u317430902_pruebas
+// $servername = "sql201.epizy.com";
+// $username   = "epiz_27965998";
+// $password   = "gTrvbP9EEXxrKyk";
+// $dbname     = "epiz_27965998_maternidad";
