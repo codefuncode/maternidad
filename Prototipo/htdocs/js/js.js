0f728e55d4def851d248ae057fc12c787f803042
alemandiazcarlos <caleman9791>
2021-09-06 21:44:33 -0400

    Llamada a funciones  comentadas  para fácil acceso a definiciones.
    Al menos se comenta la primera llamada ala función.  En el proceso se descubre la extracción de funciones independientes para la reutilización de las mismas. La importancia de este fichero es de prioridad de acciones. En el futuro se puede cancelar la llamada a la acción con condicionales de ser necesaria una modificación.

--- Prototipo/htdocs/js/js.js
+++ Prototipo/htdocs/js/js.js
@@ -1,4 +1,10 @@
+// ==========================================================
+// Se inicia después de que todo el documentado esta cargado
+// La documentado a esta función  esta en el fichero {inicio.js}
 cargada();
+// ==========================================================
+
+
 inserta_bebe();
 
 btn_vav();
